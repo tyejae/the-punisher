d028746e32dcbe43f81c16066cc09c45a7148f37
tyejae <tyejae@gmail.com>
2020-11-09 05:17:59 -0700

    to MSF Discord

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const Request = require('request');
+const cron = require('cron');
 
 const SERVERS = [
     'players-under-1m', 
@@ -15,8 +16,134 @@ const SERVERS = [
     'players-above-9m',
     'players-above-10m',
     'looking-for-alliance'];
+const GUILD_SERVER_IDS = {
+    '382255096454578186': [   // MSF.gg Discord
+        '671124282675560448', // players-under-1m 
+        '671124228958846986', // players-above-1m
+        '671124156665954304', // players-above-2m
+        '671123710371168286', // players-above-3m
+        '671123548982607897', // players-above-4m
+        '671151052413992960', // players-above-5m
+        '735583924649984090', // players-above-6m
+        '735583969877295156', // players-above-7m
+        '735584007584219230', // players-above-8m
+        '773274593607417896', // players-above-9m
+        '773274661001756695'  // players-above-10m
+    ]
+}
 
-bot.login(process.env.BOT_TOKEN);
+
+const checkForExpiredRecruitMessages = new cron.CronJob('* * * * *', async () => {
+    const expired = await getExpiredRecruits();
+    expired.forEach(recruit => {
+        const guildId = recruit.guildId;
+        const channelId = recruit.channelId;
+        const messageId = recruit.messageId;
+        const guild = bot.guilds.find(g => g.id === guildId);
+        if (guild) {
+            const channel = guild.channels.find(c => c.id === channelId);
+            if (channel) {
+                channel.fetchMessage(messageId).then(message => {
+                    if (message) {
+                        message.delete();
+                    }
+                }).catch(() => {})
+            }
+        }
+    })
+});
+
+const checkForNewRecruits = new cron.CronJob('* * * * *', async () => {
+    const recruits = await getNewRecruits();
+    recruits.forEach(recruit => {
+        const power = parseInt(recruit.power);
+        bot.guilds.array().forEach(g => {
+            let channel;
+            const servers = GUILD_SERVER_IDS[g.id];
+            if (power > 10000000) {
+                channel = g.channels.find(c => c.id === servers[10])
+            } else if (power > 9000000) {
+                channel = g.channels.find(c => c.id === servers[9])
+            } else if (power > 8000000) {
+                channel = g.channels.find(c => c.id === servers[8])
+            } else if (power > 7000000) {
+                channel = g.channels.find(c => c.id === servers[7])
+            } else if (power > 6000000) {
+                channel = g.channels.find(c => c.id === servers[6])
+            } else if (power > 5000000) {
+                channel = g.channels.find(c => c.id === servers[5])
+            } else if (power > 4000000) {
+                channel = g.channels.find(c => c.id === servers[4])
+            } else if (power > 3000000) {
+                channel = g.channels.find(c => c.id === servers[3])
+            } else if (power > 2000000) {
+                channel = g.channels.find(c => c.id === servers[2])
+            } else if (power > 1000000) {
+                channel = g.channels.find(c => c.id === servers[1])
+            } else if (power <= 1000000) {
+                channel = g.channels.find(c => c.id === servers[0])
+            }
+            if (channel) {
+                const tag = recruit.tag.replace('(MSF.gg)', '').trim();
+                channel.send({
+                    'embed': {
+                        'title': `${recruit.tag.replace('(MSF.gg)', '')} is Looking For Alliance`,
+                        'url': `https://msf.gg/recruit/${recruit.memberId}`,
+                        'description': `**Power:** ${recruit.power.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}\n**Description:**\n${decodeURIComponent(recruit.description)}\n\n*To invite ${tag} to your alliance go to https://msf.gg/recruit/${recruit.memberId}*\n*To view ${tag}'s roster go to ${recruit.rosterUrl}*`,
+                        'color': 16760576,
+                        'thumbnail': {
+                            url: 'https://msf.gg/search.png',
+                        },
+                        'footer': {
+                            'iconURL': 'https://msf.gg/msfgg-bot-36x36.png',
+                            'text': 'MSF.gg • https://msf.gg',
+                        },
+                    },
+                }).then(r => {
+                    var postBody = {
+                        url: 'https://api.tyejae.com/services/msfggbot/updateLookingForAllianceIds',
+                        body: JSON.stringify({
+                            memberId: recruit.memberId,
+                            guildId: r.guild.id,
+                            channelId: r.channel.id,
+                            messageId: r.id
+                        }),
+                        headers: {
+                            'Content-Type': 'application/x-www-form-urlencoded'
+                        }
+                    };
+                    Request.post(postBody, (error, res, body) => {
+                        console.log(body)
+                    });
+                })
+            }
+        })
+    });
+});
+
+async function getNewRecruits() {
+    return new Promise((resolve) => {
+        Request(`https://api.tyejae.com/services/msfggbot/getNewRecruits`, (error, response, body) => {
+            if (!error && response.statusCode == 200) {
+                resolve(JSON.parse(body));
+            } else {
+                resolve([]);
+            }
+        })
+    });
+}
+
+async function getExpiredRecruits() {
+    return new Promise((resolve) => {
+        Request(`https://api.tyejae.com/services/msfggbot/getExpiredRecruits`, (error, response, body) => {
+            if (!error && response.statusCode == 200) {
+                resolve(JSON.parse(body));
+            } else {
+                resolve([]);
+            }
+        })
+    });
+}
 
 function attachIsImage(msgAttach) {
     var url = msgAttach.url;
@@ -70,12 +197,14 @@ async function getPower(message) {
     return power;
 }
 
-bot.on("ready", function() {
+bot.once("ready", function() {
     console.log(`Ready as: ${bot.user.tag}`);
+    checkForNewRecruits.start();
+    checkForExpiredRecruitMessages.start();
 })
 
 bot.on('messageUpdate', (oldMessage, newMessage) => {
-    if (SERVERS.indexOf(newMessage.channel.name) > -1) {
+    if (SERVERS.indexOf(newMessage.channel.name) > -1 && !newMessage.author.bot) {
         var postBody = {
             url: 'https://api.tyejae.com/services/msfggbot/updateLookingForAlliance',
             body: JSON.stringify({
@@ -98,7 +227,7 @@ bot.on('messageUpdate', (oldMessage, newMessage) => {
 })
 
 bot.on('messageDelete', async function (msg) {
-    if (SERVERS.indexOf(msg.channel.name) > -1) {
+    if (SERVERS.indexOf(msg.channel.name) > -1 && !msg.author.bot) {
         Request.get(`https://api.tyejae.com/services/msfggbot/cancelLookingForAlliance?memberId=${msg.author.id}`);
     }
 })
@@ -108,6 +237,7 @@ bot.on('message', async function (msg) {
         console.log(`[INFO] ${msg.author.username} said, "${msg.content}"`)
     }
     if (SERVERS.indexOf(msg.channel.name) > -1) {
+        if (msg.author.bot) return;
         console.log(`[INFO - ${msg.channel.name}] ${msg.author.username} said, "${msg.content}"`)
         if (msg.mentions.members.first() && !msg.author.bot && !isAdminOrMod(msg.member)) {
             msg.channel
@@ -151,6 +281,8 @@ bot.on('message', async function (msg) {
                         memberId: msg.author.id,
                         tag: msg.author.tag,
                         power: power,
+                        guildId: msg.guild.id,
+                        channelId: msg.channel.id,
                         messageId: msg.id,
                         description: encodeURIComponent(content)
                             .replace(/!/g, '%21')
@@ -201,6 +333,8 @@ bot.on('message', async function (msg) {
                     memberId: msg.author.id,
                     tag: msg.author.tag,
                     power: power,
+                    guildId: msg.guild.id,
+                    channelId: msg.channel.id,
                     messageId: msg.id,
                     description: encodeURI(content),
                     notify: '1'
