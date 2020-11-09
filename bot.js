74e393a5fdbf0bf6129ff76ac9d725628f4aa6ab
tyejae <tyejae@gmail.com>
2020-11-09 05:26:07 -0700

    Forgot the login

--- bot.js
+++ bot.js
@@ -32,6 +32,7 @@ const GUILD_SERVER_IDS = {
     ]
 }
 
+bot.login(process.env.BOT_TOKEN);
 
 const checkForExpiredRecruitMessages = new cron.CronJob('* * * * *', async () => {
     const expired = await getExpiredRecruits();
